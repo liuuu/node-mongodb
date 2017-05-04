c0c09c1b0f4b8d90deba68758749cbc2e0fa970c
liuuu <next@yudeMacBook-Air.local>
2017-05-04 13:41:52 +0800

    add parts of the delete test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -105,3 +105,30 @@ describe('GET/todos/:id', () => {
   });
 
 });
+
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    const hexId = todos[1]._id.toHexString();
+    console.log(hexId);
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        };
+        Todo.findById(hexId).then(todo => {
+          expect(todo).toNotExist();
+          done();
+        }).catch(e => done(e));
+      })
+  });
+
+  // it('should return 404 if id is inValid', done =>  {
+  //
+  // });
+})
